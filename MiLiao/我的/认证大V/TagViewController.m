dc44ed58e8f288daeeed8c0fe07c18f5de2b08e9
caozicong <853119675@qq.com>
2018-01-09 11:53:23 +0800

    .....

--- MiLiao/我的/认证大V/TagViewController.m
+++ MiLiao/我的/认证大V/TagViewController.m
@@ -25,10 +25,16 @@
     [self.navigationController.navigationBar setBackgroundImage:[UIImage imageWithColor:[[UIColor colorWithHexString:@"FFFFFF"] colorWithAlphaComponent:1]] forBarMetrics:UIBarMetricsDefault];
     self.navigationItem.titleView=[YZNavigationTitleLabel titleLabelWithText:@"选择形象标签"];
     self.view.backgroundColor = [UIColor whiteColor];
-    _textTagCollectionView2 = [[TTGTextTagCollectionView alloc]initWithFrame:CGRectMake(20, 10, WIDTH-40, HEIGHT)];
+    UILabel *lab = [[UILabel alloc]initWithFrame:CGRectMake(0, 10, WIDTH, 20)];
+    lab.text = @"仅能选择两个";
+    lab.textAlignment = NSTextAlignmentCenter;
+    lab.font = [UIFont systemFontOfSize:12];
+    lab.textColor = [UIColor redColor];
+    [self.view addSubview:lab];
+    _textTagCollectionView2 = [[TTGTextTagCollectionView alloc]initWithFrame:CGRectMake(20, 70, WIDTH-40, 150)];
     _tags = @[
               @"情感专家",
-              @"清纯美女", @"宅男女神",@"邻家小妹",@"长发飘逸",@"长发飘逸",@"长发飘逸",@"长发飘逸",@"长发飘逸"
+              @"清纯美女", @"宅男女神",@"邻家小妹",@"长发飘逸",@"制服诱惑",@"丝袜美腿",@"声音迷人",@"时尚御姐",@"小蛮腰",@"二次元",@"大长腿",@"女汉子"
               ];
     _textTagCollectionView2.delegate = self;
     _textTagCollectionView2.showsVerticalScrollIndicator = NO;
@@ -37,18 +43,18 @@
 
     config = _textTagCollectionView2.defaultConfig;
     
-    config.tagTextFont = [UIFont systemFontOfSize:20.0f];
+    config.tagTextFont = [UIFont systemFontOfSize:13.0f];
     
     config.tagExtraSpace = CGSizeMake(12, 12);
     
     config.tagTextColor = [UIColor whiteColor];
     config.tagSelectedTextColor = [UIColor whiteColor];
     
-    config.tagBackgroundColor = [UIColor colorWithRed:0.10 green:0.53 blue:0.85 alpha:1.00];
+    config.tagBackgroundColor = [UIColor grayColor];
     config.tagSelectedBackgroundColor = [UIColor redColor];
     
     config.tagCornerRadius = 8.0f;
-    config.tagSelectedCornerRadius = 4.0f;
+    config.tagSelectedCornerRadius = 8.0f;
     
     config.tagBorderWidth = 0;
     
@@ -66,6 +72,11 @@
     [_textTagCollectionView2 addTags:_tags];
     [_textTagCollectionView2 reload];
     [self.view addSubview:_textTagCollectionView2];
+    UIButton *sure = [[UIButton alloc]initWithFrame:CGRectMake(30, CGRectGetMaxY(_textTagCollectionView2.frame)+40, WIDTH-60, 50)];
+    [sure setTitle:@"确定" forState:UIControlStateNormal];
+    sure.backgroundColor = [UIColor redColor];
+    sure.layer.cornerRadius = 8;
+    [self.view addSubview:sure];
 }
 #pragma mark - TTGTextTagCollectionViewDelegate
 
