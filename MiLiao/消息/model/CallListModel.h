05537efab385163c8738be25f02f51ed8424f619
caozicong <853119675@qq.com>
2018-01-17 18:36:51 +0800

    更新2

--- MiLiao/消息/model/CallListModel.h
+++ MiLiao/消息/model/CallListModel.h
@@ -11,4 +11,10 @@
 @interface CallListModel : NSObject
 @property (strong, nonatomic) NSString *headUrl;//头像
 @property (strong, nonatomic) NSString *createDate;//时间
+@property (strong, nonatomic) NSString *anchorAccount;
+@property (strong, nonatomic) NSString *anchorId;
+@property (strong, nonatomic) NSString *userAccount;
+@property (strong, nonatomic) NSString *userId;
+@property (strong, nonatomic) NSString *callId;
+@property (strong, nonatomic) NSString *callTime;
 @end
