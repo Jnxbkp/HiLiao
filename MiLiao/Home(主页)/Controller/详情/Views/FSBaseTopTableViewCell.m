3af02e0838e738f25f9593f92f68d23009933d3a
jarvanbin@gmail.com <1270396626@qq.com>
2018-01-24 18:24:50 +0800

    详情页点击效果空白

--- MiLiao/Home(主页)/Controller/详情/Views/FSBaseTopTableViewCell.m
+++ MiLiao/Home(主页)/Controller/详情/Views/FSBaseTopTableViewCell.m
@@ -20,6 +20,7 @@
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
         
+        self.selectionStyle = UITableViewCellSelectionStyleNone;
         self.backgroundColor = [UIColor whiteColor];
         [self addHeadView];
        
